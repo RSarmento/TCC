299359236aa4a2d51391d8b4c5a7a5211d917ea5
rivo <rivo.sarmento@audora.com.br>
2019-07-25 14:32:00 -0300

    Inclusão de outro caderno jupyter

--- pre_process/__init__.py
+++ pre_process/__init__.py
@@ -1,5 +1,8 @@
-import sentiment_analysis.sentimental as s
 import pre_process.exec as pp
+import sentiment_analysis.sentimental as s
+from nltk.classify.scikitlearn import SklearnClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
 
 
 ementas_acordaos = pp.load()
@@ -9,3 +12,6 @@ dataset = {'resultado': s.sentiment_analysis(acordao_pp), 'ementa': ementa_pp}
 pp.save(dataset)
 
 print('done.')
+
+X_train, X_test, y_train, y_test = train_test_split(ementa_pp, acordao_pp, test_size=0.2, random_state=0)
+model = SklearnClassifier(SVC(kernel='linear'))
