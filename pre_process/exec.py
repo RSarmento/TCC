636853b46120c39adb305352f0475194df539678
rivo <rivo.sarmento@audora.com.br>
2019-07-25 19:22:11 -0300

    Ajuste na raspagem

--- pre_process/exec.py
+++ pre_process/exec.py
@@ -2,7 +2,6 @@ import json
 import re
 
 import pandas as pd
-from unidecode import unidecode
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
 
@@ -24,11 +23,34 @@ def load():
     return [acordaos, ementas]
 
 
+def roman_to_int(doc):
+    import romanclass
+
+    doc
+    new_corpus = []
+    for i in doc:
+        wordlist = []
+        for j in i:
+            if j in roman:
+                wordlist.append(str(romanclass.fromRoman(j)))
+            else:
+                wordlist.append(j)
+        new_corpus.append(wordlist)
+    corpus = new_corpus
+    return doc
+
+
 def clean(corpus):
     docs = list()
+    roman = ["MMM", "MM", "M",
+             "CM", "DCCC", "DCC", "DC", "D", "CD", "CCC", "CC", "C",
+             "XC", "LXXX", "LXX", "LX", "L", "XL", "XXX", "XX", "X",
+             "IX", "VIII", "VII", "VI", "V", "IV", "III", "II", "I"]
     stemmer = SnowballStemmer("portuguese")
     for doc in corpus:
-        words = doc.lower()
+        pointless = re.sub('.', ' ', doc) # ERRADO AQUIIIIIIIIIIIIIIIIIIIIIIIIIII
+        romanless = re.sub(roman, ' ', pointless)
+        words = romanless.lower()
         numberless = re.sub(r'\d', '', words)
         superscriptless = re.sub(r'[\u00A0-\u00BF]*', '', numberless)
         words_only = unidecode(re.sub(r'\W+', ' ', superscriptless))
