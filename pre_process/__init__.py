610fcb249febd7cee60ebc61f5917f29888ba545
rivo <rivo.sarmento@audora.com.br>
2019-07-26 14:36:45 -0300

    Ajuste nas classificações usando BOW e TFIDF

--- pre_process/__init__.py
+++ pre_process/__init__.py
@@ -1,11 +1,26 @@
+from nltk import SklearnClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
 import pre_process.exec as pp
 import sentiment_analysis.sentimental as s
 
 ementas_acordaos = pp.load()
-pp.save_ementas(ementas_acordaos[1])
+
+classes_raw, ementas_acordaos[1] = pp.classes(ementas_acordaos[1])
+pp.save(classes_raw, ['classes'], 'classes_raw')
+
+classes = pp.clean(classes_raw)
+pp.save(classes, ['classes'], 'classes')
+
+pp.save(ementas_acordaos[1], ['ementas'], 'ementas')
+
 acordao_pp = pp.clean(ementas_acordaos[0])
 ementa_pp = pp.clean(ementas_acordaos[1])
 dataset = {'resultado': s.sentiment_analysis(acordao_pp), 'ementa': ementa_pp}
-pp.save(dataset)
+pp.save_dataset(dataset)
 
 print('done.')
+
+RandomForestClassifier(n_estimators=1000, random_state=0)
+KNeighborsClassifier
--- pre_process/exec.py
+++ pre_process/exec.py
@@ -26,34 +26,41 @@ def load():
     return [acordaos, ementas]
 
 
-def clean(corpus):
-    docs = list()
-    stemmer = SnowballStemmer("portuguese")
+def classes(corpus):
+    new_corpus = []
+    all_classes = []
     for doc in corpus:
+        ementa_defeituosa = re.sub("E M E N T A", "EMENTA", doc)
+        classe = re.sub('-', '.', re.sub('', '.', ementa_defeituosa).partition('.')[0]).partition('.')[0]
+        new_corpus.append(re.sub(classe, '', doc))
+        classe = classe.partition(' EM ')[0]
+        classe = classe.partition(' E ')[0]
+        classe = classe.partition(' NO ')[0]
+        classe = classe.partition(' NA ')[0]
+        all_classes.append(classe)
+    return all_classes, new_corpus
+
+
+def clean(docs):
+    new_docs = list()
+    stemmer = SnowballStemmer("portuguese")
+    for doc in docs:
         pointless = re.sub(r'[^\w\s]', '', doc).lower()
         romanless = ' '.join([word for word in pointless.split() if word not in roman.roman])
-        words = romanless.lower()
-        numberless = re.sub(r'\d', '', words)
+        numberless = re.sub(r'\d', '', romanless)
         superscriptless = re.sub(r'[\u00A0-\u00BF]*', '', numberless)
         words_only = unidecode(re.sub(r'\W+', ' ', superscriptless))
         stopwordless_words = [word for word in words_only.split() if word not in stopwords.words('portuguese')]
         stemmed_words = [stemmer.stem(word) for word in stopwordless_words]
-        docs.append(' '.join(stemmed_words))
-    return docs
+        new_docs.append(' '.join(stemmed_words))
+    return new_docs
 
 
-def save(dataset):
+def save(dataset, title, file):
+    df = pd.DataFrame(dataset, columns=title)
+    df.to_csv(r'../data/'+file+'.txt', sep='\t', index=None, header=False)
+
+
+def save_dataset(dataset):
     df = pd.DataFrame(dataset, columns=['resultado', 'ementa'])
     df.to_csv(r'../data/dataset_tratado.txt', sep='\t', index=None, header=False)
-
-
-def save_ementas(ementas):
-    df = pd.DataFrame(ementas, columns=['ementa'])
-    df.to_csv(r'../data/ementas.txt', sep='\t', index='None', header=False)
-
-
-def classes(corpus):
-    docs = []
-    for doc in corpus:
-        docs.append(doc.partition('.'))
-    return docs
