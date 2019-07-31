c10438a593667d7c0e4a7d937265928723501a11
rivo <rivo.sarmento@audora.com.br>
2019-07-31 16:16:00 -0300

    Inclusão de classificação multi-class com Doc2Vec

--- test.py
+++ test.py
@@ -1,33 +1,27 @@
-import nltk
-import pandas as pd
-from gensim.models import Word2Vec
-from nltk import tokenize
-from sklearn.model_selection import train_test_split
+from gensim.models import doc2vec
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+from tqdm import tqdm
 
-# Carregando dados
-
-df = pd.read_csv('data/dataset_classes.txt', header=None, encoding='utf-8', sep='\t')
-ementas = df[1]
-
-ementas_list = [word for word in ementas.iteritems()]
-ementas_list_list = []
-for doc in ementas_list:
-    ementas_list_list.append(doc[1].split())
-
-X = Word2Vec(ementas_list_list, min_count=2)
-X.wv.init_sims()
-print(X)
+tqdm.pandas(desc="progress-bar")
 
 
-def w2v_tokenize_text(text):
-    tokens = []
-    for sent in tokenize.word_tokenize(text, language='portuguese'):
-        for word in nltk.word_tokenize(sent, language='portuguese'):
-            if len(word) < 2:
-                continue
-            tokens.append(word)
-    return tokens
+def label_sentences(corpus, label_type):
+    """
+    Gensim's Doc2Vec implementation requires each document/paragraph to have a label associated with it.
+    We do this by using the TaggedDocument method. The format will be "TRAIN_i" or "TEST_i" where "i" is
+    a dummy index of the post.
+    """
+    labeled = []
+    for i, v in enumerate(corpus):
+        label = label_type + '_' + str(i)
+        labeled.append(doc2vec.TaggedDocument(v.split(), [label]))
+    return labeled
 
 
-train, test = train_test_split(df, test_size=0.2, random_state=0)
-tokenize.word_tokenize(train[1], language='portuguese')
+print(tqdm)
+train_test_split
+
+
+print('accuracy %s' % accuracy_score(y_pred, y_test))
+print(classification_report(y_test, y_pred,target_names=my_tags))
