9ce2ad01b87d658746c270fc94cdf99f381c8129
rivo <rivo.sarmento@audora.com.br>
2019-08-01 17:50:03 -0300

    Estudos com análise multiclasse

--- pre_process/__init__.py
+++ pre_process/__init__.py
@@ -1,10 +1,15 @@
 import pre_process.exec as pp
 import sentiment_analysis.sentimental as s
+import numpy as np
+import pandas as pd
+
 
 ementas_acordaos = pp.load()
 
 classes_raw, ementas_acordaos[1] = pp.classes(ementas_acordaos[1])
 pp.save(classes_raw, ['classes'], 'classes_raw')
+
+classes_capadas =
 
 classes = pp.clean(classes_raw)
 pp.save(classes, ['classes'], 'classes')
@@ -20,3 +25,11 @@ pp.save_dataset(dataset)
 dataset = {'classe': classes, 'ementa': ementa_pp}
 pp.save_dataset_classes(dataset)
 print('done.')
+np.histogram()
+
+
+df = pd.read_csv('data/dataset_classes.txt', header=None, encoding='utf-8', sep='\t')
+df[0].value_counts()
+
+from imblearn.under_sampling import RandomUnderSampler
+
